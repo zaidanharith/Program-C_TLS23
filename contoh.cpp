de085b543e4425a0d79e5fa4bc04c527ad2163d5
Zaidan Harith <myharithzaidan@gmail.com>
2023-09-22 09:06:53 +0700

    Replacing

--- contoh.cpp
+++ contoh.cpp
@@ -1,26 +1,34 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main(){
 
-     // Inisiasi variabel
-     double a,b,op;
+     // Inisialisasi Variabel untuk Ukuran Matriks
+     int m,n,p;
+     // Tentukan Ukuran Matriks
+     cin >> m >> n >> p;
+     // Membuat Matriks A dengan Ukuran m x n dan Matriks B dengan Ukuran n x p
+     int matriksA[m][n],matriksB[n][p];
+     // Beri Nilai untuk Semua Elemen di Matriks A
+     for(int i=0; i<m; i++){
+          for(int j=0; j<n; j++) cin >> matriksA[i][j];
+     }
+     // Beri Nilai untuk Semua Elemen di Matriks B
+     for(int i=0; i<n; i++){
+          for(int j=0; j<p; j++) cin >> matriksB[i][j];
+     }
 
-     // Input
-     cout << "Masukkan angka : ";
-     cin >> a;
-     cout << "1 : Tambah\n2 : Kurang\n3 : Kali\n4 : Bagi\nMasukkan tanda yang sesuai : ";
-     cin >> op;
-     cout << "Masukkan angka : ";
-     cin >> b;
-     
-     // Logika
-     cout << "Hasilnya adalah : ";
-     if(op == 1) cout << a+b << endl;
-     else if(op == 2) cout << a-b << endl;
-     else if(op == 3) cout << a*b << endl;
-     else if(op == 4) cout << a/b << endl;
-     else cout << "Tidak Terdefinisi" << endl;
+     // Membuat Matriks Hasil Perkalian yang disebut dengan Matriks C dengan ukuran m x p
+     int matriksC[m][p];
+     // Beri Nilai Untuk Semua Elemen di Matriks C Berdasarkan Konsep Perkalian Matriks Sekaligus Menampilkan Matriks C
+     for(int i=0; i<m; i++){
+          for(int j=0; j<p; j++){
+               matriksC[i][j]=0;
+               for(int k=0; k<n; k++) matriksC[i][j] += (matriksA[i][k] * matriksB[k][j]);
+               cout << matriksC[i][j] << " ";
+          }
+          cout << endl;
+     }
 
      return 0;
 }
